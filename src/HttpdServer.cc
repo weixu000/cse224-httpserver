09e82c91d188daad6768a623d98405d3436d27fb
Xu Wei <xu.wei.31415@gmail.com>
2019-10-08 12:19:57 -0700

    fix typo

--- src/HttpdServer.cc
+++ src/HttpdServer.cc
@@ -91,15 +91,15 @@ void HttpdServer::serverListen() {
     }
 
     for (auto rp = result; rp; rp = rp->ai_next) {
-        logger()->info("try listening on {}", sockaddrToString(*rp->ai_addr));
-
         sock = socket(rp->ai_family, rp->ai_socktype,
                       rp->ai_protocol);
         if (sock == -1)
             continue;
 
-        if (bind(sock, rp->ai_addr, rp->ai_addrlen) == 0 && listen(sock, 10) == 0)
+        if (bind(sock, rp->ai_addr, rp->ai_addrlen) == 0 && listen(sock, 10) == 0) {
+            logger()->info("listening on {}", sockaddrToString(*rp->ai_addr));
             break;                  /* Success */
+        }
 
         close(sock);
     }
