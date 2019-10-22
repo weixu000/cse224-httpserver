f3ab467e85ce34980f9f05ddfe0e6611473d9b17
Xu Wei <xu.wei.31415@gmail.com>
2019-10-22 11:42:24 -0700

    stop using stringstream

--- src/HTTPRequest.cc
+++ src/HTTPRequest.cc
@@ -1,5 +1,3 @@
-#include <sstream>
-
 #include "HTTPRequest.hpp"
 
 HTTPRequest::HTTPRequest(const std::string &header) {
@@ -7,9 +5,10 @@ HTTPRequest::HTTPRequest(const std::string &header) {
 }
 
 void HTTPRequest::parseHeader(const std::string &header) {
-    std::istringstream ss(header);
+    auto initial_line_end = header.find("\r\n");
     std::string line;
-    if (std::getline(ss, line) && line.size() && line[line.size() - 1] == '\r') {
+    if (initial_line_end != std::string::npos && initial_line_end > 0) {
+        line = header.substr(0, initial_line_end);
         auto i = line.find(' ');
         auto j = line.find(' ', i + 1);
         if (i == std::string::npos || j == std::string::npos) {
@@ -17,31 +16,30 @@ void HTTPRequest::parseHeader(const std::string &header) {
         }
         _method = line.substr(0, i);
         _uri = line.substr(i + 1, j - (i + 1));
-        _HTTPVer = line.substr(j + 1, line.size() - 1 - (j + 1));
+        _HTTPVer = line.substr(j + 1, line.size() - (j + 1));
     } else {
-        throw BadError("Bad request initial line.");
+        throw BadError("Empty request initial line.");
     }
 
-    while (std::getline(ss, line) && line.size() && line[line.size() - 1] == '\r') {
-        if (line == "\r") {
-            break;
-        }
+    auto cur_line_start = initial_line_end + 2;
+    std::string::size_type cur_line_end;
+    std::string key, value;
+    // there must be empty line here
+    while ((cur_line_end = header.find("\r\n", cur_line_start)) != std::string::npos
+           && cur_line_end > cur_line_start) {
+        line = header.substr(cur_line_start, cur_line_end - cur_line_start);
         auto i = line.find(": ");
         if (i == std::string::npos) {
             throw BadError("Bad request header fields");
         }
-        std::string key, value;
         key = line.substr(0, i);
-        value = line.substr(i + 2, line.size() - 1 - (i + 2));
+        value = line.substr(i + 2, line.size() - (i + 2));
         if (key.empty() || value.empty()) {
             throw BadError("Bad request header fields");
         }
         _fields[key] = value;
+        cur_line_start = cur_line_end + 2;
     }
-    if (line != "\r") {
-        throw BadError("No empty line at end of header");
-    }
-
     if (!_fields.count("Host")) {
         throw BadError("Host is required");
     }
